3c8923a14bcd1fbef2e7e4d2c2907e5de0a97fd6
OleksiiDovhaniuk <45332459+OleksiiDovhaniuk@users.noreply.github.com>
2020-04-20 13:29:58 +0300

    Gene is simplified. Performance >> 25%.

--- fitness_function.py
+++ fitness_function.py
@@ -5,12 +5,12 @@ calculation the fitness function of current
 genetic algorithm.
 
 Functions: 
-    generation_result(generation, inputs, outputs, coefs),
+    calculate(generation, inputs, outputs, coefs),
     garbage_outs_number(inputs, outputs),
     quantum_cost(chromosome, el_quantum_cost),
-    delay(chromosome, element_delay),
+    delay_time(chromosome, element_delay),
     elements_number(chromosome),
-    err_list(chromosome, inputs, outputs), 
+    err_no(chromosome, sgn_no inputs, outputs), 
     fredkin_result(signals), 
     fitness_function(errors, c, g, s, coefs), 
     k_from_error(errors), g_from_c(c), h_from_g(g), i_from_s(s).
@@ -19,10 +19,11 @@ Functions:
 import math
 from timeit import timeit
 
+
 FREDKIN_DELAY = 1
 FREDKIN_QUANTUM_COST = 5
 FREDKIN_INPUTS_NUMBER = 3
-ITERATION_POWER = 100
+ITERATION_POWER = 300
 
 def k_from_error(errors):
     """ Factor k based on number of errors in system.
@@ -95,7 +96,7 @@ def i_from_s(s):
         return i
 
 def fitness_function(errors, c, g, s, coefs):
-    """ Calculates fitness function value. 
+    """Calculates fitness function value. 
 
     Args: 
         errors (int): delay of circuit.
@@ -121,163 +122,108 @@ def fitness_function(errors, c, g, s, coefs):
     coefs[3] * i_from_s(s))
     return value
 
-def fredkin_result(signals):
-    """ Simulates Fredkin element action.
-
-    Args: signals (list of int): list like [C_in, I1, I2].
-    
-    Returns: 
-        [C_out, O1, O2] = [C_in, I1 XOR S, I2 XOR S],
-        where S = (I1 XOR I2) AND C.
-
-    Note:
-        Input signals should be one dimensional list with 3 elements inside.
-
-    Examples of execution:
-        >>> fredkin_result([0, 0, 0])
-        [0, 0, 0]
-        >>> fredkin_result([0, 0, 1])
-        [0, 0, 1]
-        >>> fredkin_result([0, 1, 0])
-        [0, 1, 0]
-        >>> fredkin_result([1, 0, 0])
-        [1, 0, 0]
-        >>> fredkin_result([0, 1, 1])
-        [0, 1, 1]
-        >>> fredkin_result([1, 0, 1])
-        [1, 1, 0]
-        >>> fredkin_result([1, 1, 0])
-        [1, 0, 1]
-        >>> fredkin_result([1, 1, 1])
-        [1, 1, 1]
-
-    """
-    C_out = signals[0]
-    O1    = signals[2]
-    O2    = signals[1]
-
-    PQR = [C_out]
-    if C_out == 1:
-        PQR.append(O1)
-        PQR.append(O2)
-        return PQR
-    else:
-        return signals
-
-def err_no(chromosome, inputs, outputs):
-    """ Returns errors number of schemotechnical system.
+def err_no(chromosome, sgn_no, inputs, outputs):
+    """`[Warning]: description needs to be reconsidered!` 
+    Returns errors number of schemotechnical system.
 
     Args: 
-        chromosome (3D list): individual one from generation.
-        inputs (2D tuple): input signals from truth table.
-        outputs (2D tuple): output signals from truth table.
+        chromosome (3D list): individual one from generation;
+        sgn_no (int);
+        inputs (tuple of ints): input signals from truth table,
+            decimal representations of hexidicimal digits, which 
+            represent `column` in the truth table;
+        outputs (tuple of ints): output signals from truth table,
+            decimal representations of hexidicimal digits, which 
+            represent `row` in the truth table.
     
     Returns: err_no(int): minimal number of errors in current chromosome.
 
     Note: 
-        Size of inputs and outputs lists is equal. 
-        Number of alets is equal to number of rows in inputs/outputs list.
-        Alet is look like [n (int), m (int)], 
-        where n is logic element i (n >= 0),
-        m i of input on that element (0 <= m < 3). 
+        Sizes of inputs and outputs tuples is equal. 
+        Gene is look like [n (int), m (int)], 
+        where n, m hexidicimal digits; n (in binar view) 
+        represents positions of control gates and
+        m (in binar view) represents switching gates. 
 
     Examples of execution:
-        >>> [err_no(chromosome, ins_or, outs_or) \
+        >>> [err_no(chromosome, no_or, ins_or, outs_or)\
             for chromosome in generation_or]
         [0, 0, 1, 1, 0]
-        >>> [err_no(chromosome, ins_sum, outs_sum) \
+        >>> [err_no(chromosome, no_sum, ins_sum, outs_sum) \
             for chromosome in generation_sum]
         [0, 10, 6, 0]
 
     """
-    ins_no = len(inputs)
-    outs_no = len(outputs)
-
-    # Copy inputs for local variable for safety of global values and switch rows <-> colums.
-    inputs = list(map(list, zip(*inputs)))
-    outputs = list(map(list, zip(*outputs)))
-
     err_no = 0
-    err_list = [[0] * ins_no for _ in range(outs_no)]
+    active_ins = inputs.copy()
+    once = (2 ** sgn_no) - 1
+    outs_no = len(outputs[0])
+    err_list = [[0] * sgn_no for _ in range(outs_no)]
 
+    for ins, outs in zip(active_ins, outputs): 
+        for control, switch in chromosome:
+            if control & ins == control:
+                if '{0:b}'.format(switch & ins).count('1') == 1:
+                    ins = ins ^ switch
 
-    for active_ins, cheking_outs in zip(inputs, outputs):
+        for sgn, errs in zip(outs, err_list):
+            if sgn:
+                err_segment = once ^ ins
+            else:
+                err_segment = ins
+            segment_str = '{0:b}'.format(err_segment)[::-1]
 
-        for gene in chromosome:
-            to_switch = False 
-            pos1 = pos2 = -1
+            for i in range(sgn_no):
+                try:
+                    errs[-i-1] += int(segment_str[i])
+                except IndexError:
+                    pass
 
-            for k, gate_pos in enumerate(gene):
-                if gate_pos:
-
-                    if gate_pos == 2:
-                        to_switch = bool(active_ins[k])
-
-                        if not to_switch: 
-                            break
-                        
-                        elif to_switch and (pos2 != -1):
-                            active_ins[pos1], active_ins[pos2]\
-                                = active_ins[pos2], active_ins[pos1]
-                            break
-
-                    elif pos1 == -1:
-                        pos1 = k
-                        
-                    else:
-                        pos2 = k
-
-                        if to_switch: 
-                            active_ins[pos1], active_ins[pos2]\
-                                = active_ins[pos2], active_ins[pos1]
-                            break
-
-        for index in range(outs_no):
-            for jndex in range(ins_no):
-                err_list[index][jndex] += cheking_outs[index] ^ active_ins[jndex] 
-
-    # calculate overall errors number
-    while err_list:
-        list_mins = [min(row) for row in err_list]
-
-        err_no += min(list_mins)
-        err_list.pop(list_mins.index(min(list_mins)))
+    min_list = sorted([min(row) for row in err_list])
+    for min_value in min_list[:outs_no]:
+        err_no += min_value
 
     return err_no
-
-def delay_time(chromosome, element_delay):
-    """ Returns delay value of system in ns.
+        
+def delay_time(chromosome, sgn_no,  element_delay):
+    """`[Warning]: description needs to be reconsidered!` 
+    Returns delay value of system in ns.
 
     Args: 
-        chromosome (3D list): individual of generation.
-        element_delay (float): logic gate delay in nano secunds.
-    
+        chromosome (3D list): individual of generation;
+        element_delay (float): logic gate delay in nano secunds;
+        sgn_no (int).
+
     Returns: delay (float): delay of current chromosome.
 
     Examples of execution:
-        >>> [delay_time(chromosome, 1) for chromosome in generation_or]
+        >>> [delay_time(chromosome, no_or, 1) for chromosome in generation_or]
         [1, 1, 1, 1, 2]
-        >>> [delay_time(chromosome, 3) for chromosome in generation_sum]
-        [12, 12, 18, 12]
+        >>> [delay_time(chromosome, no_sum, 3) for chromosome in generation_sum]
+        [12, 15, 18, 12]
 
     """
-    delay_list = []
-    chrm = list(map(list, zip(*chromosome))) # switch rows <-> columns
+    delay_list = [0] * sgn_no
 
-    for line in chrm:
-        delay_list.append(line.count(1))
-        delay_list[-1] += line.count(2)
+    for control, switch in chromosome:
+        element = '{0:b}'.format(control | switch)[::-1]
+        for i in range(sgn_no):
+            try:
+                delay_list[i] += int(element[i])
+            except IndexError:
+                	pass
 
     return max(delay_list) * element_delay
 
 def quantum_cost(chromosome, el_quantum_cost):
-    """ Returns quantum value of schemotechnical system.
+    """`[Warning]: description needs to be reconsidered!` 
+    Returns quantum value of schemotechnical system.
     
     Args: 
         chromosome (3D list): individual of generation.
-        el_quantum_cost (float): logic gate quantum cost.
+        el_quantum_cost (int): logic gate quantum cost.
     
-    Returns: quantum_cost (float): quantum cost of current chromosome.
+    Returns: quantum_cost (int): quantum cost of current chromosome.
 
     Examples of execution:
         >>> [quantum_cost(chromosome, 5) for chromosome in generation_or]
@@ -288,16 +234,18 @@ def quantum_cost(chromosome, el_quantum_cost):
     """
     element_no = 0
 
-    for gene in chromosome:
-       if 2 in gene: element_no +=1
+    for control, _ in chromosome:
+       if control: element_no += 1
        
     return element_no * el_quantum_cost
 
-def garbage_outs_number(signals):
-    """ Returns number of garbage outputs/ extra inputs
+def garbage_outs_number(sgn_no, inputs, outputs):
+    """`[Warning]: description needs to be reconsidered!` 
+    Returns number of garbage outputs/ extra inputs
 
     Args: 
-        inputs (2D tuple): input signals from truth table.
+        sgn_no (int): number of signals;
+        inputs (tuple of ints): input signals from truth table;
         outputs (2D tuple): output signals from truth table.
     
     Returns: elements_number (int): number of logic gates.
@@ -305,29 +253,27 @@ def garbage_outs_number(signals):
     Note: Size of inputs and outputs lists is equal. 
 
     Examples of execution:
-        >>> garbage_outs_number([ins_or, outs_or])
+        >>> garbage_outs_number(no_or, ins_or, outs_or)
         2
-        >>> garbage_outs_number([ins_sum, outs_sum])
+        >>> garbage_outs_number(no_sum, ins_sum, outs_sum)
         3
     """
-    align_no = 0 
+    align_no = len('{0:b}'.format(max(inputs)))
 
-    for row in signals[0]:
-        if row.count(row[0]) == len(row):
-            align_no += 1
+    return max(sgn_no-align_no, sgn_no-len(outputs[0]))
 
-    return max  (align_no, len(signals[0]) - len(signals[1]))
-
-def generation_result(generation, inputs, outputs, coefs):
-    """ Returns one dimensional list of fitness function values for current generation.
+def calculate(generation, sgn_no, inputs, outputs, coefs):
+    """ `[Warning]: description needs to be reconsidered!` 
+    Returns one dimensional list of fitness function values for current generation.
 
     Args: 
-        generation (4D list): current generation.
-        inputs (2D tuple): input signals from truth table.
-        outputs (2D tuple): output signals from truth table.
-        coefs (tuple of int): fitness function coeficients in order: α, ß, y, δ
+        generation (3D list): current generation;
+        sgn_no (int);
+        inputs (tuple of ints): input signals from truth table;
+        outputs (2D tuple): output signals from truth table;
+        coefs (tuple of ints): fitness function coeficients in order: α, ß, y, δ
     
-    Returns: generation_result (list): fitness function values for each chromosome.
+    Returns: calculate (list): fitness function values for each chromosome.
 
     Note: 
         Size of inputs and outputs lists is equal. 
@@ -337,63 +283,80 @@ def generation_result(generation, inputs, outputs, coefs):
         m i of input on that element (0 <= m < 3). 
 
     Examples of execution:
-        >>> [round(result, 3) for result in generation_result(generation_or, \
+        >>> [round(result, 3) for result in calculate(generation_or, no_or,\
             ins_or, outs_or, coefs)]
         [0.978, 0.978, 0.528, 0.528, 0.963]
-        >>> [round(result, 3) for result in generation_result(generation_sum, \
+        >>> [round(result, 3) for result in calculate(generation_sum, no_sum,\
             ins_sum, outs_sum, coefs)]
-        [0.945, 0.125, 0.169, 0.943]
+        [0.945, 0.123, 0.169, 0.943]
     """
     results = []
 
     for chromosome in generation:
-        e = err_no(chromosome, inputs, outputs)
+        e = err_no(chromosome, sgn_no, inputs, outputs)
         c = quantum_cost(chromosome, FREDKIN_QUANTUM_COST)
-        g = garbage_outs_number([inputs, outputs])
-        s = delay_time(chromosome, FREDKIN_DELAY)
+        g = garbage_outs_number(sgn_no, inputs, outputs)
+        s = delay_time(chromosome, sgn_no, FREDKIN_DELAY)
          
         results.append(fitness_function(e, c, g, s, coefs))
 
     return results
 
-TEST={
+__values__ = {
     'coefs': [0.9, 0.034, 0.033, 0.033],
+    'no_or': 3,
+    'no_sum': 6,
+    # 'ins_or': [[0, 0, 1, 1],  
+    #            [0, 1, 0, 1],  
+    #            [1, 1, 1, 1]],
 
-    'ins_or': [[0, 0, 1, 1],  
-               [0, 1, 0, 1],  
-               [1, 1, 1, 1]],
+    # 'outs_or': [[0, 1, 1, 1]], 
 
-    'outs_or': [[0, 1, 1, 1]], 
-
-    'generation_or': [[[2, 1, 1]],
+    # 'generation_or': [[[2, 1, 1]],
                        
-                      [[0, 0, 0],
-                       [1, 2, 1],
-                       [0, 0, 0]],
+    #                   [[0, 0, 0],
+    #                    [1, 2, 1],
+    #                    [0, 0, 0]],
 
-                      [[1, 1, 2],
-                       [0, 0, 0],
-                       [0, 0, 0]],
+    #                   [[1, 1, 2],
+    #                    [0, 0, 0],
+    #                    [0, 0, 0]],
                        
-                      [[0, 0, 0],
-                       [1, 1, 2]],
+    #                   [[0, 0, 0],
+    #                    [1, 1, 2]],
                        
-                      [[0, 0, 0],
-                       [1, 1, 2],
-                       [0, 0, 0],
-                       [2, 1, 1],
-                       [0, 0, 0]]],
+    #                   [[0, 0, 0],
+    #                    [1, 1, 2],
+    #                    [0, 0, 0],
+    #                    [2, 1, 1],
+    #                    [0, 0, 0]]],
 
-    'outs_sum': [[0, 1, 1, 0, 1, 0, 0, 1], 
-                 [0, 0, 0, 1, 0, 1, 1, 1],
-                 [0, 0, 1, 1, 1, 1, 0, 0]],
+    'ins_or': [1, 3, 5, 7],
 
-    'ins_sum': [[0, 0, 0, 0, 1, 1, 1, 1],
-                [0, 0, 1, 1, 0, 0, 1, 1],
-                [0, 1, 0, 1, 0, 1, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [1, 1, 1, 1, 1, 1, 1, 1]],
+    'outs_or': [[0], [1], [1], [1]], 
+
+    'generation_or':[
+        [[4,3]],
+                       
+        [[0,0], [2,5], [0,0]],
+
+        [[1,6], [0,0], [0,0]],
+                       
+        [[0,0], [1,6]],
+                       
+        [[0,0], [1,6], [0,0], [4,3], [0,0]]
+    ],
+
+    # 'outs_sum': [[0, 1, 1, 0, 1, 0, 0, 1], 
+    #              [0, 0, 0, 1, 0, 1, 1, 1],
+    #              [0, 0, 1, 1, 1, 1, 0, 0]],
+
+    # 'ins_sum': [[0, 0, 0, 0, 1, 1, 1, 1],
+    #             [0, 0, 1, 1, 0, 0, 1, 1],
+    #             [0, 1, 0, 1, 0, 1, 0, 1],
+    #             [1, 1, 1, 1, 1, 1, 1, 1],
+    #             [0, 0, 0, 0, 0, 0, 0, 0],
+    #             [1, 1, 1, 1, 1, 1, 1, 1]],
 
     'big_outs': [[0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1], 
                  [0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1],
@@ -412,25 +375,25 @@ TEST={
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
 
-    # 'outs_sum': [[0, 0, 0, 2, 2, 2],
-    #              [1, 0, 0, 2, 2, 2],
-    #              [1, 0, 1, 2, 2, 2],
-    #              [0, 1, 1, 2, 2, 2],
-    #              [1, 0, 1, 2, 2, 2],
-    #              [0, 1, 1, 2, 2, 2],
-    #              [0, 1, 0, 2, 2, 2],
-    #              [1, 1, 0, 2, 2, 2]],
+    'outs_sum_': [[0, 0, 0, 2, 2, 2],
+                 [1, 0, 0, 2, 2, 2],
+                 [1, 0, 1, 2, 2, 2],
+                 [0, 1, 1, 2, 2, 2],
+                 [1, 0, 1, 2, 2, 2],
+                 [0, 1, 1, 2, 2, 2],
+                 [0, 1, 0, 2, 2, 2],
+                 [1, 1, 0, 2, 2, 2]],
 
-    # 'ins_sum': [[0, 0, 0, 1, 0, 1],
-    #             [0, 0, 1, 1, 0, 1],
-    #             [0, 1, 0, 1, 0, 1],
-    #             [0, 1, 1, 1, 0, 1],
-    #             [1, 0, 0, 1, 0, 1],
-    #             [1, 0, 1, 1, 0, 1],
-    #             [1, 1, 0, 1, 0, 1],
-    #             [1, 1, 1, 1, 0, 1]],
+    'ins_sum_': [[0, 0, 0, 1, 0, 1],
+                [0, 0, 1, 1, 0, 1],
+                [0, 1, 0, 1, 0, 1],
+                [0, 1, 1, 1, 0, 1],
+                [1, 0, 0, 1, 0, 1],
+                [1, 0, 1, 1, 0, 1],
+                [1, 1, 0, 1, 0, 1],
+                [1, 1, 1, 1, 0, 1]],
 
-    'generation_sum': [[[0, 2, 0, 0, 1, 1],
+    'generation_sum_': [[[0, 2, 0, 0, 1, 1],
                         [0, 0, 0, 0, 0, 0],
                         [2, 1, 0, 0, 0, 1],
                         [1, 0, 0, 1, 2, 0],
@@ -479,6 +442,41 @@ TEST={
                         [1, 0, 0, 2, 1, 0],
                         [0, 1, 1, 0, 0, 2]]],
 
+    'ins_sum': [5, 13, 21, 29, 37, 45, 53, 61],
+
+    # 'outs_sum': [105, 23, 60],
+
+    # 'outs_sum': [[0, 1, 1, 0, 1, 0, 0, 1], 
+    #              [0, 0, 0, 1, 0, 1, 1, 1],
+    #              [0, 0, 1, 1, 1, 1, 0, 0]],
+
+    'outs_sum': [[0, 0, 0],
+                 [1, 0, 0],
+                 [1, 0, 1],
+                 [0, 1, 1],
+                 [1, 0, 1],
+                 [0, 1, 1],
+                 [0, 1, 0],
+                 [1, 1, 0]],
+
+    # 'outs_sum': [0, 4, 5, 3, 5, 3, 2, 6],
+
+    # 'outs_sum': [[0, 1, 1, 0, 1, 0, 0, 1], 
+    #              [0, 0, 0, 1, 0, 1, 1, 1],
+    #              [0, 0, 1, 1, 1, 1, 0, 0]],
+
+    'generation_sum':[
+        [[16, 3], [ 0, 0], [32,17], [ 2,36], [16, 9], [ 0, 0], [ 1,40]],
+
+        [[16,12], [ 0, 0], [ 2,33], [ 0, 0], [ 1,34], [ 0, 0], [16,12], [ 0, 0], 
+         [32,24], [ 0, 0], [ 8, 3], [ 1,36], [ 0, 0], [ 8,18]],
+
+        [[16, 9], [32, 9], [32, 6], [ 1,24], [ 4,33], [16,10], [ 1,34], [16,12],
+         [ 1,48]],
+
+        [[32, 3], [ 0, 0], [ 4,24], [ 0, 0], [ 8, 3], [ 0, 0], [32,12], [ 2,17],
+         [ 0, 0], [ 0, 0], [ 0, 0], [ 0, 0], [ 0, 0], [ 4,34], [ 1,24]]
+    ],
     'big_generation': [[[0, 2, 1, 1, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [1, 0, 0, 0, 2, 1, 0, 0, 0],
@@ -532,13 +530,13 @@ TEST={
                         [1, 0, 0, 1, 0, 2, 0, 0, 0],
                         [0, 0, 0, 0, 0, 2, 1, 1, 0],
                         [0, 1, 2, 0, 1, 0, 0, 0, 0]]]
-
     }
-
-def test_err_(): err_no(TEST['big_generation'][1], TEST['big_ins'], TEST['big_outs']) 
-
-print(timeit(test_err_, number=ITERATION_POWER))
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod(extraglobs=TEST)
+    doctest.testmod(extraglobs=__values__)
+
+def test_err():
+    err_no(__values__['generation_sum'][0], __values__['no_sum'], __values__['ins_sum'], __values__['outs_sum'])
+
+print(timeit(test_err, number=ITERATION_POWER))
