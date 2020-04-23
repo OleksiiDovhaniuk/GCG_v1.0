b02754028adf73b81e6904fff165e15de59c9381
OleksiiDovhaniuk <45332459+OleksiiDovhaniuk@users.noreply.github.com>
2020-04-23 23:36:53 +0300

    Tests and comments

--- file_work.py
+++ file_work.py
@@ -14,79 +14,96 @@
         read(path),
         autosave(data).
 """
-import datetime
 import json
 import os
 import os.path
+from datetime import datetime
 
 
+STORAGE_PATH = "saves/storage/"
 DEFAULT_PATH = "saves/data.json"
 AUTOSAVE_PATH = "saves/autosaves.json"
 DEFAULT_DATA ={
-    'Algorithm Configurations':{
+    'Algorithm':{
         'name': 'Genetic',
         'configurations':{
             'generation size':{
                 'value': 400,
-                'type': int,
-                'min_value': 'memorised number',
-                'max_value': 9999,
+                'type': 'int',
+                'min': 'memorised number',
+                'max': 9999,
                 'is active': True,
                 'group': 'root'
             },
             'chromosome size':{
                 'value': 7,
-                'type': int,
-                'min_value': 2,
-                'max_value': 99,
+                'type': 'int',
+                'min': 2,
+                'max': 99,
                 'is active': True,
                 'group': 'root'
             },
             'gene size':{
                 'value': 6,
-                'type': int,
-                'min_value': 2,
-                'max_value': 99,
+                'type': 'int',
+                'min': 2,
+                'max': 99,
+                'is active': True,
+                'group': 'root'
+            },
+            'memorised number':{
+                'value': 5,
+                'type': 'int',
+                'min': 2,
+                'max': 'generation size',
+                'is active': True,
+                'group': 'root'
+            },
+            'control gates':{
+                'value': [1, 1],
+                'type': 'list of ints',
+                'min': 1,
+                'max': 'gene size',
                 'is active': True,
                 'group': 'root'
             },
             'crossover probability':{
                 'value': .2,
-                'type': float,
-                'min_value': 0,
-                'max_value': 1,
+                'type': 'float',
+                'min': 0,
+                'max': 1,
                 'is active': True,
                 'group': 'root'
             },
             'mutation probability':{
                 'value': .02,
-                'type': float,
-                'min_value': 0,
-                'max_value': 1,
+                'type': 'float',
+                'min': 0,
+                'max': 1,
                 'is active': True,
                 'group': 'root'
             },
             'fitness function coeficients':{
                 'value': [.91, .03, .03, .03],
-                'type': list,
-                'min_value': 0,
-                'max_value': 1,
+                'type': 'list of float',
+                'min': 0,
+                'max': 1,
                 'is active': True,
                 'group': 'root'
             },
             'process time':{
                 'value': 600,
-                'type': int,
-                'min_value': 1,
-                'max_value': 99999999999,
+                'type': 'int',
+                'min': 1,
+                'max': 99999999999,
                 'is active': True,
                 'group': 'process limits'
             },
             'iterations limit':{
                 'value': 1000,
-                'type': int,
-                'min_value': 1,
-                'max_value': 99999999999,
+                'type': 'int',
+                'min': 1,
+                'max': 99999999999,
                 'is active': False,
                 'group': 'process limits'
             },
@@ -112,37 +129,43 @@ DEFAULT_DATA ={
     },
 }
 
-_max_autosaves_no = 20
-_autosave_names = []
+_max_autosaves_no = 5
 
-def save(data, path=DEFAULT_PATH, indent=4):
+def save(data, path=DEFAULT_PATH):
     """ Function saves  configurations` data from .json file.
 
     Args: 
         data: nested dictionary;
         path (str);
-        indent (int).
 
+     Examples of execution:
+        >>> save(DEFAULT_DATA, 'saves/test.json')
+        >>> save(DEFAULT_DATA, 'D:1/2/saves/test2.json')
+        [Errno 2] No such file or directory: 'D:1/2/saves/test2.json'
     """
     try:
-        f = open(path, 'w+')
-        f.write(json.dumps(data, sort_keys=False, indent=indent))
-        f.close()
+        with open(path, 'w+') as f:
+            json.dump(data, f, indent=4, sort_keys=False)
 
     except IOError as e:
         print(e)
-    
-   
+
 def read(path=DEFAULT_PATH):
     """ Function reads the configurations` data from .json file.
 
     Args: path (str).
 
+    Examples of execution:
+        >>> read('saves/test.json')['Algorithm']['name']
+        'Genetic'
+        >>> read('D:1/2/saves/test2.json')['Truth Table']['outputs']['S']
+        [Errno 2] No such file or directory: 'D:1/2/saves/test2.json'
+        '01101001'
+
     """
     try:
-        f = open(path, 'r')
-        if f.mode == 'r':
-            return json.loads(f.read)
+        with open(path, 'r') as f:
+            return json.load(f)
 
     except IOError as e:
         print(e)
@@ -164,15 +187,20 @@ def autosave(data):
         data: nested dictionary;
         path (str).
 
+    Examples of execution:
+        >>> autosave(DEFAULT_DATA)
+        >>> saves = read(AUTOSAVE_PATH)
+        >>> saves[[key for key in saves][0]]['Truth Table']['outputs']['P']
+        '00111100'
     """
     autosaves = read(AUTOSAVE_PATH)
-
-    key = str(datetime.now())
-    _autosave_names.append(key)
-    autosaves[key] = data
+    autosaves[str(datetime.now())] = data
     
-    while len(autosaves > _max_autosaves_no):
-        del autosaves[_autosave_names[0]]
-        _autosave_names.pop(0)
+    while len(autosaves) > _max_autosaves_no:
+        del autosaves[[key for key in autosaves][0]]
         
-    save(autosaves, AUTOSAVE_PATH, 5)
+    save(autosaves, AUTOSAVE_PATH)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
